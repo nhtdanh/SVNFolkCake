b82bc2a3285063f5d6eb1c9b0a613c8403a86637
nhtdanh <kn10196@gmail.com>
2025-08-25 07:27:44 +0700

    Cấu hình deploy

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,5 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'primeicons/primeicons.css'
 
@@ -8,18 +7,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 
-
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 import Nora from '@primeuix/themes/aura' // chọn theme Aura (sáng, hiện đại)
 
-
 import App from './App.vue'
 import './assets/style.css'
 
-
 import VueCookies from 'vue-cookies'
-
 
 const app = createApp(App)
 
